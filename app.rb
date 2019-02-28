038c800b97f678072eed623240765df6afa491c4
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-02-28 16:54:32 +0000

    test for udpating a bookmark, failing

--- app.rb
+++ app.rb
@@ -20,6 +20,20 @@ class Bookmarks < Sinatra::Base
     Bookmark.delete(params[:id])
     redirect ('/bookmarks')
   end
+
+  get '/bookmarks/:id/update' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    @bookmark = connection.exec("select * from bookmarks where id = #{params['id']}")
+    @bookmark = @bookmark.first
+    erb :'bookmarks/edit'
+  end
+
+  post '/bookmarks/:id/patch' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    conection.exec("update bookmarks set url = '#{params['url']}', title = '#{params['title']}' where id = '#{params['id']}'")
+    redirect '/bookmarks'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
